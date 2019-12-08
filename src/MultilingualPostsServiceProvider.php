179ba6ee848d3cbc06dcb00547161bcc5810b9fb
Michael Joyner <joyner.michael@gmail.com>
2019-12-08 10:48:00 +0800

    update service provider

--- src/MultilingualPostsServiceProvider.php
+++ src/MultilingualPostsServiceProvider.php
@@ -30,6 +30,12 @@ class MultilingualPostsServiceProvider extends ServiceProvider
                 __DIR__.'/../database/migrations/create_multilingual_category_post_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_multilingual_category_post_table.php'),
             ], 'migrations');
         }
+
+        if (! class_exists('CreateMultilingualPostsMediaModelsTable')) {
+            $this->publishes([
+                __DIR__.'/../database/migrations/create_multilingual_posts_media_models_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_multilingual_posts_media_models_table.php'),
+            ], 'migrations');
+        }
     }
 
     public function register()
