51a673fd6b5af3d041cda6bafe2efca07caf6549
Michael Joyner <joyner.michael@gmail.com>
2020-04-05 17:17:53 +0800

    bug fix

--- src/PostImagesController.php
+++ src/PostImagesController.php
@@ -18,7 +18,7 @@ class PostImagesController extends Controller
         $image = Post::findOrFail($postId)->attachImage(request('image'));
 
         $conversion = ImageConversions::useForPostImageUploadResponse();
-        $url = config('multilingual_posts.use_full_url_for_body_images', false) ?
+        $url = config('multilingual-posts.use_full_url_for_body_images', false) ?
         $image->getFullUrl($conversion) : $image->getUrl($conversion);
 
         return response(['src' => $url], 201);
